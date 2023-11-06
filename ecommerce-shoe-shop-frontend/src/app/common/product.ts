3374da700ee0f33e768325bd8866f4460a1fd360
Tóth Márton Marcell <hun.nort@gmail.com>
2023-11-06 18:00:44 +0100

    set right path to images in product.ts and added the actual images

--- ecommerce-shoe-shop-frontend/src/app/common/product.ts
+++ ecommerce-shoe-shop-frontend/src/app/common/product.ts
@@ -1,11 +1,11 @@
 export class Product {
 
-    public imgUrl: string = "image-coming-soon-placeholder.png";
-
-    constructor(public brand:string, 
+    constructor(
+        public brand:string, 
         public model: string,
         public color: string,
         public releaseDate: Date,
         public price: number,
-        public description: string){}
+        public description: string,
+        public imgUrl: string) {}
 }
