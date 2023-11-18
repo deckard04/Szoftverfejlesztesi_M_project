424eb3e1f0102e481ca2b30a886ae59944c2d4b7
Mitró György Krisztián <mitrogyuri0421@gmail.com>
2023-11-18 13:18:48 +0100

    search bar component and search added

--- ecommerce-shoe-shop-frontend/src/app/services/product.service.ts
+++ ecommerce-shoe-shop-frontend/src/app/services/product.service.ts
@@ -12,10 +12,26 @@ export class ProductService {
 
   constructor(private httpClient: HttpClient) { }
 
+  getSpecificProductList(categoryId: string): Observable<Product[]>{
+    const searchUrl = `${this.url}/search/findByBrand?id=${categoryId}`;
+    return this.getProducts(searchUrl);
+  }
+
   getProductList(): Observable<Product[]>{
     return this.httpClient.get<GetResponse>(this.url).pipe(
       map(elements => elements._embedded.products)
     );
+  }
+
+  getSearchResult(searchKeyword: string): Observable<Product[]>{
+    const searchUrl = `${this.url}/search/findByModelContaining?name=${searchKeyword}`;
+    return this.getProducts(searchUrl);
+  }
+
+  getProducts(searchUrl: string): Observable<Product[]>{
+    return this.httpClient.get<GetResponse>(searchUrl).pipe(
+      map(elements => elements._embedded.products)
+    )
   }
 }
 
