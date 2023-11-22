c1a26fe4c5c3e9512724b4deb36ef08b980e9c33
Mitró György Krisztián <mitrogyuri0421@gmail.com>
2023-11-22 17:22:26 +0100

    cart CRUD functions, and cart view added

--- ecommerce-shoe-shop-frontend/src/app/services/cart.service.ts
+++ ecommerce-shoe-shop-frontend/src/app/services/cart.service.ts
@@ -1,12 +1,30 @@
 import { Injectable } from '@angular/core';
 import { CartItem } from '../common/cart-item';
-import { Subject } from 'rxjs';
+import { BehaviorSubject, Subject } from 'rxjs';
+import { LocalStorageService } from 'ngx-webstorage';
+import { StorageService } from 'ngx-webstorage/lib/core/interfaces/storageService';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  decrementQuantity(cartItem: CartItem) {
+    cartItem.quantity--;
+
+    if (cartItem.quantity === 0){
+      this.removeItemFromCart(cartItem);
+    }
+    else{
+      this.computeCartTotals();
+    }
+  }
+  
+  removeItemFromCart(cartItem: CartItem) {
+    let index = this.cartItems.findIndex(tempCartItem => tempCartItem.id == cartItem.id && tempCartItem.sizeId === cartItem.sizeId);
+    this.cartItems.splice(index, 1);
+    this.computeCartTotals();
+  }
 
   
   cartItems: CartItem[] = [];
