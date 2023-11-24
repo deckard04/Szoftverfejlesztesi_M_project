5255c2b6843c8bfcd1882974df16118e41522dac
Mitró György Krisztián <mitrogyuri0421@gmail.com>
2023-11-24 12:45:35 +0100

    karma conf file and coverage analise files added, and some validators

--- ecommerce-shoe-shop-frontend/src/app/components/checkout/checkout.component.ts
+++ ecommerce-shoe-shop-frontend/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CartService } from 'src/app/services/cart.service';
+import { Shopvalidator } from 'src/app/validators/shopvalidator';
 
 @Component({
   selector: 'app-checkout',
@@ -10,15 +12,23 @@ export class CheckoutComponent implements OnInit{
 
   checkoutFormGroup!: FormGroup;
 
-  constructor(private formBuilder: FormBuilder){}
+  totalPrice: number = 0;
+  totalQuantity: number = 0;
+
+  constructor(private formBuilder: FormBuilder,
+    private cartService: CartService){}
 
 
   ngOnInit(): void {
+    
+    this.reviewCartDetails();
+
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
-        firstName: [''],
-        lastName: [''],
-        email: ['']
+        firstName: new FormControl('', [Validators.required, Validators.minLength(2), Shopvalidator.notOnlyWhiteSpace]),
+        lastName: new FormControl('', [Validators.required, Validators.minLength(2), Shopvalidator.notOnlyWhiteSpace]),
+        phone: new FormControl('', [Validators.required, Validators.pattern('^(?:\\+36|06)(?:(20|30|31|50|70)\\d{7})$')]),
+        email: new FormControl('', [Validators.required, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')])
       }),
       shippingAddress: this.formBuilder.group({
         street: [''],
@@ -29,8 +39,36 @@ export class CheckoutComponent implements OnInit{
       })
     });
   }
+  reviewCartDetails() {
+    this.cartService.totalPrice.subscribe(
+      data => this.totalPrice = data
+    )
+    this.cartService.totalQuantity.subscribe(
+      data => this.totalQuantity = data
+    )
+  }
 
+  get firstName(){
+    return this.checkoutFormGroup.get('customer.firstName');
+  }
+
+  get lastName(){
+    return this.checkoutFormGroup.get('customer.lastName');
+  }
+
+  get email(){
+    return this.checkoutFormGroup.get('customer.email');
+  }
+
+  get phone(){
+    return this.checkoutFormGroup.get('customer.phone');
+  }
   onSubmit(){
+
+    if (this.checkoutFormGroup.invalid){
+      this.checkoutFormGroup.markAllAsTouched();
+    }
+
     console.log(this.checkoutFormGroup.get('customer')?.value);
     console.log(this.checkoutFormGroup.get('shippingAddress')?.value);
   }
---
+++ ecommerce-shoe-shop-frontend/src/app/validators/shopvalidator.spec.ts
@@ -0,0 +1,7 @@
+import { Shopvalidator } from './shopvalidator';
+
+describe('Shopvalidator', () => {
+  it('should create an instance', () => {
+    expect(new Shopvalidator()).toBeTruthy();
+  });
+});
